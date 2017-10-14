7157efdfa02e4ec21850651e2dbf0a5ee4d7bea1
oo7186 <oo7186@caelumBSB03-09>
2017-10-14 17:08:20 -0300

    Aula 5

--- src/br/com/caelum/contas/main/TestaIO.java
+++ src/br/com/caelum/contas/main/TestaIO.java
@@ -4,17 +4,22 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URL;
 import java.util.Scanner;
 
 public class TestaIO {
 	public static void main(String[] args) throws Exception{
-		InputStream is = System.in;
+		URL url = new URL("http://www.caelum.com.br"); //lendo da internet
+		//InputStream is = System.in; // ler do teclado
+		//InputStream is = new FileInputStream("/home/oo7186/teste.txt"); // ler de arquivo
+		InputStream is = url.openStream();
 		Scanner sc = new Scanner(is);
-		OutputStream os = new FileOutputStream("/home/oo7186/teste.txt");
+		OutputStream os = new FileOutputStream("/home/oo7186/saida.txt");
 		PrintStream ps = new PrintStream(os);
 		while (sc.hasNextLine()){
 			String linha = sc.nextLine();
-			linha = linha.toUpperCase();
+			//linha = linha.toUpperCase();
+			linha = linha.toLowerCase().replace("e", "*");
 			ps.println(linha);
 		}
 		sc.close();
